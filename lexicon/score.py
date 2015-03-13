0246d6c205255f370a733c91f7c5eb9f2f637091
Milosz Wielondek <miloszw@kth.se>
2015-03-13 11:11:23 +0100

    (feat) add argv flags to scoring script for customized run settings

--- lexicon/score.py
+++ lexicon/score.py
@@ -4,27 +4,37 @@ MAIN_PATH = "../data/aclImdb/test"
 POS_PATH = MAIN_PATH + "/pos"
 NEG_PATH = MAIN_PATH + "/neg"
 
-def score(analyze):
+def score(analyzer, tests=[]):
   import operator
   from glob import glob
 
+  # default tests both positive and negative
+  if not tests:
+    tests = ['pos','neg']
+
   for label, path, op in [('pos', POS_PATH, operator.gt), ('neg', NEG_PATH, operator.lt)]:
+    if label not in tests:
+      continue
     correct = counter = 0
     for file in glob(path+"/*.txt"):
       # skip hidden / system files
-      if file.startswith("."):
+      if file.startswith("\."):
         continue
       with open(file, 'r') as f:
-        if op(analyze(f.read()),0):
+        if op(analyzer(f.read()),0):
           correct += 1
         counter += 1
-    print "%s --> Correct: %d Tested: %d Ratio: %f" % \
-      (label, correct, counter, float(correct)/counter)
+    if counter < 1:
+      print "%s ---> no tests were run." % label
+    else:
+      print "%s --> Correct: %d Tested: %d Ratio: %f" % \
+        (label, correct, counter, float(correct)/counter)
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    print "Usage: %s <package.analyzer>"
+    print "Usage: %s <package.analyzer> [tests]" % sys.argv[0]
     sys.exit(1)
   module,func = sys.argv[1].split('.')
   analyzer = eval("__import__('%s').%s" % (module,func))
-  score(analyzer)
+  tests = sys.argv[2:]
+  score(analyzer, tests)
