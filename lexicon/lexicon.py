6eb6e0652b55dec1e706a9bce2b9ea56d97bf16e
Milosz Wielondek <miloszw@kth.se>
2015-03-17 12:54:03 +0100

    (style) convert tabs to spaces

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,36 +1,36 @@
 """
-	Features:
-		* Simple summing of sentiment words (-5 .. 5)
-		* Checking for negation words in the vincinity of each sentiment words
+    Features:
+        * Simple summing of sentiment words (-5 .. 5)
+        * Checking for negation words in the vincinity of each sentiment words
 """
 class Lexicon(object):
 
-	# the polarity of sentiment words is shifted if a
-	# negation word is within NEGATION.RANGE of that word
-	NEGATION_RANGE = 8
+    # the polarity of sentiment words is shifted if a
+    # negation word is within NEGATION.RANGE of that word
+    NEGATION_RANGE = 8
 
-	# use afinn-111 as dictionary
-	with open("../data/afinn/AFINN-111.txt", "r") as afinn:
-		sentiment_dictionary = dict(map(lambda (k,v): \
-			(k,int(v)),[line.split('\t') for line in afinn]))
+    # use afinn-111 as dictionary
+    with open("../data/afinn/AFINN-111.txt", "r") as afinn:
+        sentiment_dictionary = dict(map(lambda (k,v): \
+            (k,int(v)),[line.split('\t') for line in afinn]))
 
-	with open("../data/common/negation.txt", "r") as neg:
-		negation_list = neg.read().splitlines()
+    with open("../data/common/negation.txt", "r") as neg:
+        negation_list = neg.read().splitlines()
 
-	def analyze(self, snippet):
-		words = snippet.lower().split()
-		sum = 0
-		negate_flag = False
-		negate_counter = 0
-		for word in words:
-			if word in self.negation_list:
-				negate_flag = True
-				negate_counter = 0
-				continue
-			negate_counter += 1
-			if negate_counter >= self.NEGATION_RANGE:
-				negate_flag = False
-			sentiment_value = self.sentiment_dictionary.get(word, 0)
-			sum += sentiment_value if not negate_flag else -1 * sentiment_value
+    def analyze(self, snippet):
+        words = snippet.lower().split()
+        sum = 0
+        negate_flag = False
+        negate_counter = 0
+        for word in words:
+            if word in self.negation_list:
+                negate_flag = True
+                negate_counter = 0
+                continue
+            negate_counter += 1
+            if negate_counter >= self.NEGATION_RANGE:
+                negate_flag = False
+            sentiment_value = self.sentiment_dictionary.get(word, 0)
+            sum += sentiment_value if not negate_flag else -1 * sentiment_value
 
-		return sum
+        return sum
