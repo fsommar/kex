191fa53be596625f97073db958fdbd6e8a8f6ab6
Milosz Wielondek <miloszw@kth.se>
2015-03-17 12:13:44 +0100

    (feat) add negation word support to lexicon

---
+++ lexicon/lexicon_v1.py
@@ -0,0 +1,12 @@
+"""
+	Features:
+		* Simple summing of weighted words (-5 .. 5)
+"""
+class Lexicon(object):
+
+	# use afinn-111 as dictionary
+	with open("../data/afinn/AFINN-111.txt", "r") as afinn:
+		dictionary = dict(map(lambda (k,v): (k,int(v)),[line.split('\t') for line in afinn]))
+
+	def analyze(self, sentence):
+		return sum(map(lambda word: self.dictionary.get(word, 0), sentence.lower().split()))
