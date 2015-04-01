8d2a4082ecc6e55b93c3196e6f8566a143d66093
Milosz Wielondek <miloszw@kth.se>
2015-04-01 17:01:28 +0200

    (feat) add working implementation of the svm classifier

--- learning/learning.py
+++ learning/learning.py
@@ -1,30 +1,25 @@
 import prune_data
 import numpy as np
+from sklearn import  metrics
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.cross_validation import train_test_split
 
 def analyze():
-    # data, targets, target_names = prune_data.prune()
+    data, targets, target_names = prune_data.prune()
 
-    data = [
-        u"Hello i like ice cream",
-        u"Me too i love ice cream",
-        u"i really hate ice cream"
-    ]
-    targets = [0,0,1]
-    target_names = ['pos','neg']
-    classify(data, targets, target_names, data)
+    classify(data, targets, target_names)
 
 
-def classify(data, targets, target_names, test_data):
-    print type(targets)
+def classify(data, target, target_names):
+    train_data, test_data, train_target, test_target  = train_test_split(data, target, test_size=0.5)
     svm_clf = Pipeline([('vect', CountVectorizer()),
                         ('tfidf', TfidfTransformer()),
                         ('svm', LinearSVC())])
-    svm_clf = svm_clf.fit(data, targets)
+    svm_clf = svm_clf.fit(train_data, train_target)
     svm_predicted = svm_clf.predict(test_data)
-    print(metrics.classification_report(targets, svm_predicted,
+    print(metrics.classification_report(test_target, svm_predicted,
         target_names=target_names))
 
 if __name__ == "__main__":
